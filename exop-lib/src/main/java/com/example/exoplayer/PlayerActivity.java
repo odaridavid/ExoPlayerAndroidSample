2f4b74d7d003cdff7b859e928e6ccad17d808aa6
David Odari <davidkibzodari@gmail.com>
2018-09-29 12:08:36 +0300

    Initial Commit

---
+++ exop-lib/src/main/java/com/example/exoplayer/PlayerActivity.java
@@ -0,0 +1,231 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.exoplayer;
+
+import android.annotation.SuppressLint;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.DefaultRenderersFactory;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.Player;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.Timeline;
+import com.google.android.exoplayer2.analytics.AnalyticsListener;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.source.ConcatenatingMediaSource;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.dash.DashChunkSource;
+import com.google.android.exoplayer2.source.dash.DashMediaSource;
+import com.google.android.exoplayer2.source.dash.DefaultDashChunkSource;
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelection;
+import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
+
+
+/**
+ * A fullscreen activity to play audio or video streams.
+ */
+public class PlayerActivity extends AppCompatActivity {
+
+    private static final String TAG = PlayerActivity.class.getSimpleName();
+    private PlayerView mPlayerView;
+    private SimpleExoPlayer player;
+    private Boolean playWhenReady = true;
+    private int currentWindow = 0;
+    private long playbackPosition = 0;
+    private static final DefaultBandwidthMeter BANDWIDTH_METER =
+            new DefaultBandwidthMeter();
+    private ComponentListener componentListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player);
+        //Reference to Exo player view
+        mPlayerView = findViewById(R.id.video_player_view);
+        componentListener = new ComponentListener();
+    }
+
+    private void initializePlayer() {
+        if (player == null) {
+            // a factory to create an AdaptiveVideoTrackSelection
+            //This makes sure the bandwidth meter is informed about downloaded bytes
+            // and enables to collect data for bandwidth estimation.
+            TrackSelection.Factory adaptiveTrackSelectionFactory =
+                    new AdaptiveTrackSelection.Factory(BANDWIDTH_METER);
+            player = ExoPlayerFactory.newSimpleInstance(this,
+                    new DefaultRenderersFactory(this),
+                    new DefaultTrackSelector(adaptiveTrackSelectionFactory),
+                    new DefaultLoadControl());
+        }
+        mPlayerView.setPlayer(player);
+        player.setPlayWhenReady(playWhenReady);
+        player.seekTo(currentWindow, playbackPosition);
+        //(R.string.media_url_mp3,mp4));
+        Uri uri = Uri.parse(getString(R.string.media_url_dash));
+        MediaSource mediaSource = buildMediaSource(uri);
+        player.prepare(mediaSource, true, false);
+        player.addListener(componentListener);
+        player.addAnalyticsListener(componentListener);
+    }
+
+    /**
+     * @param uri uri to media source to be built in extractor media source factory
+     * @return returns the media source to be played
+     */
+    private MediaSource buildMediaSources(Uri uri) {
+
+        // these are reused for both media sources we create below
+        DefaultExtractorsFactory extractorsFactory =
+                new DefaultExtractorsFactory();
+        DefaultHttpDataSourceFactory dataSourceFactory =
+                new DefaultHttpDataSourceFactory("user-agent");
+
+        ExtractorMediaSource videoSource =
+                new ExtractorMediaSource.Factory(
+                        new DefaultHttpDataSourceFactory("exoplayer-codelab")).
+                        createMediaSource(uri);
+
+        Uri audioUri = Uri.parse(getString(R.string.media_url_mp3));
+        ExtractorMediaSource audioSource =
+                new ExtractorMediaSource.Factory(
+                        new DefaultHttpDataSourceFactory("exoplayer-codelab")).
+                        createMediaSource(audioUri);
+//        return new ExtractorMediaSource.Factory(
+//                new DefaultHttpDataSourceFactory("exoplayer-codelab")).
+//                createMediaSource(uri);
+        return new ConcatenatingMediaSource(audioSource, videoSource);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (Util.SDK_INT > 23) {
+            initializePlayer();
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        hideSystemUi();
+        if ((Util.SDK_INT <= 23 || player == null)) {
+            initializePlayer();
+        }
+    }
+
+    @SuppressLint("InlinedApi")
+    private void hideSystemUi() {
+        mPlayerView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (Util.SDK_INT <= 23) {
+            releasePlayer();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (Util.SDK_INT > 23) {
+            releasePlayer();
+        }
+    }
+
+    private void releasePlayer() {
+        if (player != null) {
+            playbackPosition = player.getCurrentPosition();
+            currentWindow = player.getCurrentWindowIndex();
+            playWhenReady = player.getPlayWhenReady();
+            player.removeListener(componentListener);
+            player.removeVideoListener(null);
+            player.removeAnalyticsListener(componentListener);
+            player.release();
+            player = null;
+        }
+    }
+
+    /**
+     * DASH is a widely used adaptive streaming format.
+     * Streaming DASH with ExoPlayer, means building an appropriate adaptive MediaSource.
+     * To switch our app to DASH we build a DashMediaSource by changing our buildMediaSource method as follows:
+     *
+     * @param uri
+     * @return
+     */
+    private MediaSource buildMediaSource(Uri uri) {
+        DataSource.Factory manifestDataSourceFactory =
+                new DefaultHttpDataSourceFactory("ua");
+        DashChunkSource.Factory dashChunkSourceFactory =
+                new DefaultDashChunkSource.Factory(
+                        new DefaultHttpDataSourceFactory("ua", BANDWIDTH_METER));
+        return new DashMediaSource.Factory(dashChunkSourceFactory,
+                manifestDataSourceFactory).createMediaSource(uri);
+    }
+
+    private class ComponentListener implements Player.EventListener, AnalyticsListener {
+
+        @Override
+        public void onTimelineChanged(Timeline timeline, @Nullable Object manifest, int reason) {
+
+        }
+
+        @Override
+        public void onPlayerStateChanged(boolean playWhenReady,
+                                         int playbackState) {
+            String stateString;
+            switch (playbackState) {
+                case Player.STATE_IDLE:
+                    stateString = "ExoPlayer.STATE_IDLE      -";
+                    break;
+                case Player.STATE_BUFFERING:
+                    stateString = "ExoPlayer.STATE_BUFFERING -";
+                    break;
+                case Player.STATE_READY:
+                    stateString = "ExoPlayer.STATE_READY     -";
+                    break;
+                case Player.STATE_ENDED:
+                    stateString = "ExoPlayer.STATE_ENDED     -";
+                    break;
+                default:
+                    stateString = "UNKNOWN_STATE             -";
+                    break;
+            }
+            Log.d(TAG, "changed state to " + stateString
+                    + " playWhenReady: " + playWhenReady);
+        }
+    }
+}
